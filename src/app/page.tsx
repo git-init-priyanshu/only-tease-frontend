63f5170644f9bac4929821a56f8d32d965ff79ba
Priyanshu <bartwalpriyanshu00@gmail.com>
2024-06-25 17:52:28 +0530

    Mobile view fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,15 +10,15 @@ export default function HomePage() {
   const navigation = useRouter()
   return (
     <main className='text-[#AAAAAA] font-bold bg-[#F7F2FA] flex overflow-hidden items-start h-screen relative w-full'>
-      <div className='absolute top-0 left-0 h-full w-auto'>
+      <div className='absolute top-0 left-0 h-full w-auto hidden md:block'>
         <Image src="/images/onchain.webp" className='h-full w-auto' width={600} height={200} alt='onchain' />
       </div>
-      <div className='flex items-center flex-col pt-10 min-h-screen mx-auto md:ml-[30vw] lg:ml-[50vw]'>
-        <div className='flex items-start space-x-6 justify-center z-10'>
+      <div className='flex items-center flex-col md:pt-10 min-h-screen mx-auto md:ml-[30vw] lg:ml-[50vw]'>
+        <div className='hidden items-start space-x-6 justify-center z-10 md:flex'>
           <Image src="/images/Onchain-text.webp" className='aspect-[969/148]' width={300} height={100} alt='onchain' />
           <Image src="/images/summer.webp" className='aspect-[969/148]' width={300} height={100} alt='onchain' />
         </div>
-        <div className='p-10 z-20 bg-card_bg rounded-[40px] my-10'>
+        <div className='p-10 z-20 bg-card_bg h-screen md:rounded-[40px] md:h-auto md:my-10'>
           <div className='w-[296px] h-[70px] mx-auto relative'>
             <Image src="/images/onlyteaselogo.webp" fill alt='logo' />
           </div>
